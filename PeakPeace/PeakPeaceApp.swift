6245d5c0201f505a30e7c69458243c41800ad47c
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-14 12:17:03 -0700

    Initial Commit

---
+++ PeakPeace/PeakPeaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakPeaceApp.swift
+//  PeakPeace
+//
+//  Created by Rodion Akhmedov on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PeakPeaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
