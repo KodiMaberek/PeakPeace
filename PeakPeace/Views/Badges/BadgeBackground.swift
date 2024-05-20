323a10e2237b0df444f270b69b8bec6ed2121029
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-20 11:10:57 -0700

    Hexagon shape

--- PeakPeace/Views/Badges/BadgeBackground.swift
+++ PeakPeace/Views/Badges/BadgeBackground.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct BadgeBackground: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { reader in
+            Path { path in
+                var width: CGFloat = min(reader.size.width, reader.size.height)
+                let height = width
+                let xScale = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95 + xOffset,
+                        y: height * (0.20 + HexagonParametrs.adjustment)
+                    )
+                )
+                
+                HexagonParametrs.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x + xOffset,
+                            y: height * segment.line.y
+                        )
+                    )
+                    
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x + xOffset,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x + xOffset,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
+            }
+            .fill(.linearGradient(Gradient(colors: [Self.colorStart, Self.colorEnd]), startPoint: UnitPoint(x: 0.50, y: 0.00), endPoint: UnitPoint(x: 0.50, y: 0.60)))
+        }
+        .aspectRatio(1, contentMode: .fit)
     }
+    static let colorStart = Color(red: 239 / 255, green: 120 / 255, blue: 221 / 255)
+    static let colorEnd = Color(red: 239 / 255, green: 172 / 255, blue: 120 / 255)
 }
 
 #Preview {
