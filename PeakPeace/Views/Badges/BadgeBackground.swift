26ab9203716f45cea18a314ac6a2e5f3748fddc1
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-20 11:08:47 -0700

    Hexagon parameters with points for coordinate system

---
+++ PeakPeace/Views/Badges/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  PeakPeace
+//
+//  Created by Rodion Akhmedov on 5/20/24.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
