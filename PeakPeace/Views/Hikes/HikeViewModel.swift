f892b42dd334bb4429c76fbca85322129eb56e14
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-21 13:22:04 -0700

    ViewModel to store all the logic of hikes

--- PeakPeace/Views/Hikes/HikeViewModel.swift
+++ PeakPeace/Views/Hikes/HikeViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+final class HikeViewModel: ObservableObject {
+    @Published var hikes: [Hike] = loadData("hikeData.json")
+    @Published var showingDetails = false
+    
+    func rangeOfRanges<C: Collection>(_ ranges: C) -> Range<Double>
+    where C.Element == Range<Double> {
+        guard !ranges.isEmpty else { return 0..<0 }
+        let low = ranges.lazy.map { $0.lowerBound }.min()!
+        let high = ranges.lazy.map { $0.upperBound }.max()!
+        return low..<high
+    }
+    
+    func magnitude(of range: Range<Double>) -> Double {
+        range.upperBound - range.lowerBound
+    }
+    
+}
