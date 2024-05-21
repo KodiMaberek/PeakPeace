c277da3aa22e613cc8d93ff4761f25c3f9c2ce82
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-21 13:18:52 -0700

    Custom transition

--- PeakPeace/Extenshions/Transition+Ext.swift
+++ PeakPeace/Extenshions/Transition+Ext.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        .asymmetric(insertion: move(edge: .trailing).combined(with: .opacity),
+                    removal: .scale.combined(with: opacity))
+    }
+}
