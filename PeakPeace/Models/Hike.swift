c96562bf2a8cd5ad9c4b86709a929fef24ac637a
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-21 13:19:12 -0700

    Create model for hike

--- PeakPeace/Models/Hike.swift
+++ PeakPeace/Models/Hike.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var difficulty: Int
+    var distance: Double
+    var observations: [Observations]
+    
+    static var formatter = LengthFormatter()
+    
+    var formatterText: String {
+        Hike.formatter.string(
+            fromValue: distance, unit: .kilometer)
+    }
+    
+    struct Observations: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
