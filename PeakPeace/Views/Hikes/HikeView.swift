56f7a622477a32911f46b63ada4850ffde91612d
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-21 13:28:08 -0700

    Hike view

--- PeakPeace/Views/Hikes/HikeView.swift
+++ PeakPeace/Views/Hikes/HikeView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct HikeView: View {
+    @EnvironmentObject var vm: HikeViewModel
+    var hike: Hike
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(spacing: 10) {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+                
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.subheadline)
+                    Text(hike.formatterText)
+                }
+                
+                Spacer()
+                
+                Button {
+                    withAnimation {
+                        vm.showingDetails.toggle()
+                    }
+                } label: {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(vm.showingDetails ? 90 : 0))
+                        .scaleEffect(vm.showingDetails ? 1.5 : 1)
+                        .padding()
+                }
+            }
+            
+            if vm.showingDetails {
+                HikeDetails(hike: hike)
+                    .transition(.moveAndFade)
+            }
+        }
+        Spacer()
     }
 }
 
 #Preview {
-    HikeView()
+    var vm = HikeViewModel().hikes[0]
+    return HikeView(hike: vm)
+        .environmentObject(HikeViewModel())
 }
