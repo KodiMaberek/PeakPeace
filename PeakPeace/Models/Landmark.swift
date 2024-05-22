8737abadce040b84d1d9f075708779582fab0885
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-22 12:05:14 -0700

    Landmark's model

--- PeakPeace/Models/Landmark.swift
+++ PeakPeace/Models/Landmark.swift
@@ -17,6 +17,14 @@ struct Landmark: Codable, Hashable, Identifiable {
     var park: String
     var description: String
     var isFavorite: Bool
+    var isFeatured: Bool
+    
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case rivers = "Rivers"
+        case lakes = "Lakes"
+        case mountains = "Mountains"
+    }
     
     private var imageName: String
     var image: Image {
