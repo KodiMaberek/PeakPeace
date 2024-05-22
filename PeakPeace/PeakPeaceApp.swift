a99ca40b4b21909fcc949c66486ed8271ea845cb
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-22 12:14:45 -0700

    App for hiking

--- PeakPeace/PeakPeaceApp.swift
+++ PeakPeace/PeakPeaceApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct PeakPeaceApp: App {
+    let viewModel = LandmarkViewModel()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PeakPeaceTabView()
+                .environmentObject(viewModel)
         }
     }
 }
