5f8c0caf1f81b3c889c5a6ac954780549df28be0
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-30 12:47:12 -0700

    Add cover-up TabBar

--- PeakPeace/Views/Landmark/Categories/CategoryList.swift
+++ PeakPeace/Views/Landmark/Categories/CategoryList.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct CategoryList: View {
+    @Environment(\.dismiss) var presentationMode
+    @EnvironmentObject var vmTab: TabViewViewModel
+    var categoryName: String
+    var landmark: [Landmark]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(landmark) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        }
+        .hiddenTabBar(value: vmTab.showingTabBar)
+        .navigationBarBackButtonHidden(true)
+        
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    presentationMode()
+                    vmTab.showingTabBar = true
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .foregroundStyle(.gray)
+                }
+            }
+        }
+        
+        .onAppear(perform: {
+            vmTab.showingTabBar = false
+        })
+        .navigationTitle(categoryName)
+        .navigationBarTitleDisplayMode(.automatic)
     }
 }
 
 #Preview {
-    CategoryList()
+    let vm = LandmarkViewModel()
+    return CategoryList(categoryName: vm.landmarks[0].category.rawValue, landmark: Array(vm.landmarks.prefix(4)))
+        .environmentObject(vm)
+        .environmentObject(TabViewViewModel())
 }
