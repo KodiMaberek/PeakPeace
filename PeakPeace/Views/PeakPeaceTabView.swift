2a3dff3cf44c0ca6d08e9e29d9de8baa8afbca47
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-30 12:33:56 -0700

    Tab item for transition between screens

--- PeakPeace/Views/PeakPeaceTabView.swift
+++ PeakPeace/Views/PeakPeaceTabView.swift
@@ -8,33 +8,34 @@
 import SwiftUI
 
 struct PeakPeaceTabView: View {
-    @State var selection: Tab = .group
-    
-    enum Tab {
-        case group
-        case list
-    }
+    @EnvironmentObject var vm: TabViewViewModel
     
     var body: some View {
         TabView {
             CategoryHome()
-                .tag(Tab.group)
+                .tag(TabViewViewModel.Tab.group)
                 .tabItem {
                     Image(systemName: "rectangle.3.group")
                     Text("Group")
                 }
             
-            LandmarkList()
-                .tag(Tab.list)
+            FavoriteList()
+                .tag(TabViewViewModel.Tab.favorites)
                 .tabItem {
-                    Image(systemName: "list.bullet")
-                    Text("List")
+                    Image(systemName: "star")
+                    Text("Favorites")
                 }
+        }
+        .hiddenTabBar(value: vm.showingTabBar)
+        .toolbar(vm.showingTabBar ? .visible : .hidden, for: .tabBar)
+        .onAppear {
+            vm.showingTabBar = true
         }
     }
 }
 
 #Preview {
     PeakPeaceTabView()
+        .environmentObject(TabViewViewModel())
         .environmentObject(LandmarkViewModel())
 }
