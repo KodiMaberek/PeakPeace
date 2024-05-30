82575b5c290ee46c278f5976893a8facb3df574a
Kodi Maberek <kodi.maberek23@gmail.com>
2024-05-30 12:43:33 -0700

    Main category view

--- PeakPeace/Views/Landmark/Categories/CategoryHome.swift
+++ PeakPeace/Views/Landmark/Categories/CategoryHome.swift
@@ -9,29 +9,31 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var vm: LandmarkViewModel
+    
     var body: some View {
+        
         NavigationView {
             List {
                 TabView {
-                    ForEach(vm.featured, id: \.self) { feature in
+                    ForEach(vm.featured, id: \.id) { feature in
                         feature.image
                             .resizable()
                             .scaledToFill()
                             .frame(height: 200)
-                            .clipShape(.rect(cornerRadius: 8))
+                            .clipShape(.rect(cornerRadius: 10))
                             .clipped()
                     }
                 }
                 .frame(height: 200)
                 .tabViewStyle(PageTabViewStyle())
                 
-                
                 ForEach(vm.category.keys.sorted(), id: \.self) { name in
                     CategoryRow(categoryName: name, items: vm.category[name]!)
                 }
             }
             .listStyle(.inset)
-            .navigationTitle("Featured")
+            .navigationTitle("Landmarks")
+            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
